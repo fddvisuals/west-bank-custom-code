60ff564d5019bf43eddf63afd655ac821c035334
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2022-11-25 15:09:43 +0530

    Update mapbox-custom-code.js

--- mapbox-custom-code.js
+++ mapbox-custom-code.js
@@ -387,15 +387,15 @@ map.on("load", () => {
       map.setFilter("heatmap", allfilter);
     } else {
       if (globalisclicked == 1) {
+        map.setFilter("data-driven-circles", mnthfilter);
+        map.setFilter("heatmap", mnthfilter);
         map.setFilter("data-driven-circles", bluefilter1);
         map.setFilter("heatmap", bluefilter1);
+      } else if (globalisclicked == 2) {
         map.setFilter("data-driven-circles", mnthfilter);
         map.setFilter("heatmap", mnthfilter);
-      } else if (globalisclicked == 2) {
         map.setFilter("data-driven-circles", redfilter1);
         map.setFilter("heatmap", redfilter1);
-        map.setFilter("data-driven-circles", mnthfilter);
-        map.setFilter("heatmap", mnthfilter);
       } else {
         console.log("else");
         map.setFilter("data-driven-circles", mnthfilter);
