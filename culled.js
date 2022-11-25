2a9fe1ae39a2ba41ad199ffcaf2e84faf7ebc4ff
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2022-11-25 15:48:53 +0530

    Update culled.js

---
+++ culled.js
@@ -0,0 +1,22 @@
+// console.log(globalisclicked);
+    // console.log(globalvariablemonth);
+    // if (month == "11") {
+    //   map.setFilter("data-driven-circles", allfilter);
+    //   map.setFilter("heatmap", allfilter);
+    // } else {
+    //   if (globalisclicked == 1) {
+    //     map.setFilter("data-driven-circles", mnthfilter);
+    //     map.setFilter("heatmap", mnthfilter);
+    //     map.setFilter("data-driven-circles", bluefilter1);
+    //     map.setFilter("heatmap", bluefilter1);
+    //   } else if (globalisclicked == 2) {
+    //     map.setFilter("data-driven-circles", mnthfilter);
+    //     map.setFilter("heatmap", mnthfilter);
+    //     map.setFilter("data-driven-circles", redfilter1);
+    //     map.setFilter("heatmap", redfilter1);
+    //   } else {
+    //     console.log("else");
+    //     map.setFilter("data-driven-circles", mnthfilter);
+    //     map.setFilter("heatmap", mnthfilter);
+    //   }
+    // }
