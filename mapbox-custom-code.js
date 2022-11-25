035d90e602d90d9778a467b0a232bc5780e29154
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2022-11-25 14:04:44 +0530

    Update mapbox-custom-code.js

--- mapbox-custom-code.js
+++ mapbox-custom-code.js
@@ -316,7 +316,12 @@ map.on("load", () => {
       ],
       // ["match", ["get", "mm"], [globalvariablemonth], true, false],
     ];
-
+    filterBy(11);
+    document.getElementById("slider").addEventListener("input", (e) => {
+      const month = parseInt(e.target.value, 10);
+      filterBy(month);
+      globalThis.globalvariablemonth = month + 1;
+    });
     blue.onclick = function (e) {
       red.className = "filter-button red";
       this.className = "filter-button bluew active";
