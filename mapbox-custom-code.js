da998b19851956f8a2882387c880beff7123fe5a
FDD Visuals <104753762+fddvisuals@users.noreply.github.com>
2022-12-08 19:10:34 +0530

    Update mapbox-custom-code.js

--- mapbox-custom-code.js
+++ mapbox-custom-code.js
@@ -299,7 +299,6 @@ map.on("load", () => {
       this.className = "filter-button red active";
       if (globalvariablemonth != 12) {
         map.setFilter("data-driven-circles", redfilter);
-        map.setFilter("heatmap", redfilter);
       } else {
         map.setFilter("data-driven-circles", redall);
         map.setFilter("heatmap", redall);
